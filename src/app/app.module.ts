c77b03bf45a1e983de672e5fd005703cbfdb1539
Maarten Knapen <maarten.knapen@jidoka.be>
2019-10-25 11:06:49 +0200

    added firebase authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,23 @@ import {AppRoutingModule} from './modules/app-routing/app-routing.module';
 import {HeaderComponent} from './header/header.component';
 import {MaterialModule} from './modules/material/material.module';
 import {CharacterDetailDialogComponent} from './character-detail-dialog/character-detail-dialog.component';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {ComicOverviewComponent} from './comic-overview/comic-overview.component';
 import {CreatorOverviewComponent} from './creator-overview/creator-overview.component';
 import {LoaderComponent} from './utilities/loader/loader.component';
 import {LoaderInterceptor} from './interceptors/loader.interceptor';
 import {ComicDetailDialogComponent} from './comic-detail-dialog/comic-detail-dialog.component';
+import {AngularFireModule} from '@angular/fire';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import {firebaseConfig} from '../environments/environment';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+import {UserComponent} from './user/user.component';
+import {AuthService} from './services/auth.service';
+import {UserService} from './services/user.service';
+import {UserResolver} from './user/user.resolver';
+import {AuthGuard} from './guards/auth.guard';
 
 @NgModule({
   declarations: [
@@ -26,7 +37,10 @@ import {ComicDetailDialogComponent} from './comic-detail-dialog/comic-detail-dia
     ComicOverviewComponent,
     CreatorOverviewComponent,
     LoaderComponent,
-    ComicDetailDialogComponent
+    ComicDetailDialogComponent,
+    LoginComponent,
+    RegisterComponent,
+    UserComponent
   ],
   imports: [
     HttpClientModule,
@@ -34,14 +48,21 @@ import {ComicDetailDialogComponent} from './comic-detail-dialog/comic-detail-dia
     MenuModule,
     AppRoutingModule,
     MaterialModule,
-
     FormsModule,
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    ReactiveFormsModule
   ],
   providers: [
-    {provide: HTTP_INTERCEPTORS, useClass: LoaderInterceptor, multi: true}
+    {provide: HTTP_INTERCEPTORS, useClass: LoaderInterceptor, multi: true},
+    AuthService,
+    UserService,
+    UserResolver,
+    AuthGuard
   ],
   bootstrap: [AppComponent],
   entryComponents: [CharacterDetailDialogComponent, ComicDetailDialogComponent]
 })
-export class AppModule {
-}
+
+export class AppModule {}
--- src/app/modules/app-routing/app-routing.module.ts
+++ src/app/modules/app-routing/app-routing.module.ts
@@ -4,19 +4,26 @@ import {CharacterOverviewComponent} from '../../character-overview/character-ove
 import {DashboardComponent} from '../../dashboard/dashboard.component';
 import {ComicOverviewComponent} from '../../comic-overview/comic-overview.component';
 import {CreatorOverviewComponent} from '../../creator-overview/creator-overview.component';
-import {LoaderComponent} from '../../utilities/loader/loader.component';
+import {LoginComponent} from '../../login/login.component';
+import {AuthGuard} from '../../guards/auth.guard';
+import {RegisterComponent} from '../../register/register.component';
+import {UserComponent} from '../../user/user.component';
+import {UserResolver} from '../../user/user.resolver';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-  {path: 'characters', component: CharacterOverviewComponent},
-  {path: 'comics', component: ComicOverviewComponent},
-  {path: 'comics/character/:characterId', component: ComicOverviewComponent},
-  {path: 'dashboard', component: DashboardComponent},
-  {path: 'creators', component: CreatorOverviewComponent}
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent, canActivate: [AuthGuard]},
+  {path: 'characters', component: CharacterOverviewComponent, canActivate: [AuthGuard]},
+  {path: 'comics', component: ComicOverviewComponent, canActivate: [AuthGuard]},
+  {path: 'comics/character/:characterId', component: ComicOverviewComponent, canActivate: [AuthGuard]},
+  {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
+  {path: 'creators', component: CreatorOverviewComponent, canActivate: [AuthGuard]},
+  {path: 'register', component: RegisterComponent, canActivate: [AuthGuard]},
+  {path: 'user', component: UserComponent, resolve: {data: UserResolver}}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {onSameUrlNavigation: 'reload'})],
+  imports: [RouterModule.forRoot(routes, {onSameUrlNavigation: 'reload', useHash: false})],
   exports: [RouterModule]
 })
 
